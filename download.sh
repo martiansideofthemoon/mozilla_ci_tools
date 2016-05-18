1909d46d6ff1d99e2f58113414589f7603573b05
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-05-19 02:39:45 +0530

    Adding commands to save allthethings.json locally

--- download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ ! -f ./test/allthethings.json ]; then
-    wget https://secure.pub.build.mozilla.org/builddata/reports/allthethings/allthethings.20160516060001._87b285bcdaf0_21165e565d41_186a28cf7485.json -O ./test/fixtures/allthethings.json
-fi
